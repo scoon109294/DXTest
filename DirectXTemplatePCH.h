bc3581ad0b938ded4a38c681ca5ca613c63e3cdf
scoon <sgb122@gmail.com>
2015-06-12 16:46:38 +0800

    新建

---
+++ DirectXTemplatePCH.h
@@ -0,0 +1,12 @@
+//system includes
+#include <windows.h>
+
+//Directx includes
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+
+//STL includes
+#include <iostream>
+#include <string>
